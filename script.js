8892ce797fa3737b231f250adc673b8d84678564
Judith Ogbole <judithogbole71@gmail.com>
2025-06-20 01:08:32 +0100

    form creation validation

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const form = document.getElementsById('registration-form')
+
+    const feedbackDiv = this.documentElement.getElementById('form-feedback')
+
+    const userNameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+});
