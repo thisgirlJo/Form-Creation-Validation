cedafe7fc03caabbcc56abf027b89fef547b66d2
Judith Ogbole <judithogbole71@gmail.com>
2025-06-20 02:40:33 +0100

    form creation validation

--- script.js
+++ script.js
@@ -4,11 +4,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const feedbackDiv = this.documentElement.getElementById('form-feedback')
 
-    const userNameInput = document.getElementById('username');
-    const emailInput = document.getElementById('email');
-    const passwordInput = document.getElementById('password');
-
     form.addEventListener('submit', function(Event) {
         Event.preventDefault();
     });
+
+    let isValid = true;
+
+    const messages = [];
+
+    // Validate Username
+    const userNameInput = document.getElementById('username');
+    const username = userNameInput.value.trim();
+    if (username.length < 3) {
+        isValid = false;
+        messages.push("Length of Username must be more than 3 characters");
+    }
+
+    // Validate Email 
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value.trim();
+    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email) {
+        isValid = false;
+        messages.push("Invaild Email");
+    }
+
+    // Validate Password
+    const passwordInput = document.getElementById('password');
+    const password = passwordInput.value.trim();
+    if (password.length >= 8) {
+        isValid = false;
+        messages.push("Password must have at least 8 characters");
+    }
+
+
 });
